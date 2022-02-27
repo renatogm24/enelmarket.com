4f897137a1bc5bfe1eae409e81ef00d5bd18a939
renatogm24 <renato.garay@pucp.pe>
2022-02-26 20:51:20 -0500

    Delete frontend

--- frontend/components/PageNotice/index.js
+++
@@ -1,9 +0,0 @@
-function PageNotice({ text }) {
-  return (
-    <div className="flex-auto flex flex-col h-full justify-center text-center">
-      <p className="text-gray-500 text-2xl">{text}</p>
-    </div>
-  );
-}
-
-export default PageNotice;
--- frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-const withTM = require("@vercel/edge-functions-ui/transpile")();
-
-const withImages = require("next-images");
-module.exports = withImages();
-
-module.exports = withTM();
